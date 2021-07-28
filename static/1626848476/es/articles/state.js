ff6b371b477590fc35c2f2a4219c6ce601e0f80a
abk-machine <abk-machine@protonmail.com>
2021-07-28 17:00:41 +0800

    Updates

--- static/1626848476/es/articles/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"https:\u002F\u002Fabk-machine.github.io\u002Fstatic\u002F1626848476",layout:"default",error:null,serverRendered:false,routePath:"\u002Fes\u002Farticles",config:{_app:{basePath:a,assetsPath:a,cdnURL:"https:\u002F\u002Fabk-machine.github.io\u002F"},content:{dbHash:"ee3d3d1f"}}}}("\u002F"));
---
+++ static/1627462814/projects/chile-project/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"https:\u002F\u002Fabk-machine.github.io\u002Fstatic\u002F1627462814",layout:"default",error:null,serverRendered:true,routePath:"\u002Fprojects\u002Fchile-project",config:{_app:{basePath:a,assetsPath:a,cdnURL:"https:\u002F\u002Fabk-machine.github.io\u002F"},content:{dbHash:"cf293893"}}}}("\u002F"));
